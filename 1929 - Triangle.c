434d7c0911f3cff0954bf9ce1bb394ba88830545
touhid <136336143+touhid0101@users.noreply.github.com>
2026-09-01 08:44:18 +0600

    partial Solve 1929

--- 1929 - Triangle.c
+++ 1929 - Triangle.c
@@ -1,16 +1,27 @@
 // 1929 - Triangle - Beecrowd
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
     // taking length of 4 rods as input
-    int rod1, rod2, rod3, rod4;
-    scanf("%d %d %d %d", &rod1, &rod2, &rod3, &rod4);
+    int rod[4];
+    scanf("%d %d %d %d", &rod[0], &rod[1], &rod[2], &rod[3]);
 
-    if (rod1 + rod2 > rod3 || // clang-format off
-        rod1 + rod2 > rod4 ||
-        rod2 + rod3 > rod1 ||
-        rod2 + rod3 > rod4) {
+    // sorting the rods in chronological order
+    for (int i = 0, swap_count = 1; i < 4; i++) {
+        if (swap_count != 1) {
+            for (int j = 0; j < 3; j++) {
+
+            }
+        } else {
+            break;
+        }
+    }
+
+    if () {
         printf("S\n");
+    } else {
+        printf("N\n");
     }
 
     return 0;
